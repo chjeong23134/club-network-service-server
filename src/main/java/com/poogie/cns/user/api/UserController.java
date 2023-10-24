1e4e9b26f92bb7135ceb9409c3559b34ca576b02
chjeong23134 <chjeong23134@gmail.com>
2023-10-25 06:56:32 +0900

    feat : 룸 및 유저 Controller에서 CORS 설정

--- src/main/java/com/poogie/cns/user/api/UserController.java
+++ src/main/java/com/poogie/cns/user/api/UserController.java
@@ -5,12 +5,10 @@ import com.poogie.cns.user.domain.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
